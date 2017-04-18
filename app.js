fa153635db614a3554714cd5685fa2eb240a4373
John <jackbull328@gmail.com>
2017-04-17 21:12:33 -0700

    added examples for api endpoint

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ new Vue({
     fetchEvents: function() {
       var events = [
         {
-          id: 1,ready
+          id: 1,
           name: 'TIFF',
           description: 'Toronto International Film Festival',
           date: '2015-09-10'
@@ -45,6 +45,34 @@ new Vue({
         this.events.push(this.event);
         this.event = {name: '', description: '', date: ''};
       }
+    },
+
+    deleteEvent: function(index) {
+      if(confirm("Are you sure you want to delete this event?")) {
+        this.events.splice(index, 1);
+      }
     }
   }
+
+  //I added these as potential examples of what restful routs would look like
+
+  // this.$http.get('api/events').success(function(events) {
+  //   this.$set('events', events);
+  // }).error(function(error) {
+  //     console.log("Get Error api.events===>>> " + error);
+  // });
+  //
+  // this.$http.get('api/events', this.event).success(function(response) {
+  //   this.events.push(this.event);
+  //   console.log("Event added");
+  // }).error(function(error) {
+  //   console.log("Post Error api.events===>>>" + error);
+  // });
+  //
+  // this.$http.delete('api/events/' + event.id).success(fucntion(response) {
+  //   this.events.$remove(index);
+  // }).error(function(error) {
+  //   console.log(error);
+  // });
+
 });
